fcbd6b3caefa6280b41e27e5d794495ef5f511e2
Teddy Groves <groves.teddy@gmail.com>
2025-05-13 11:04:20 +0200

    Normal model with near-hardcoded error

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -16,17 +16,31 @@ TREATMENT_CODES = {
     "Enzyme": "E",
     "Saline": "S",
 }
-MODEL_FORMULA = "{y} ~ 1 + (1|mouse) + (1|treatment)"
+MODEL_FORMULA = "{y} ~ 1 + (1|treatment) + (1|mouse) + (1|roi)"
+# SIGMA_FORMULA = "sigma ~ {x_sigma}"
+
 YCOLS = [
-    "logit_alpha",
-    "log_c0",
+    "alpha",
+    "c0",
     "cr",
-    "log_k1",
-    "log_k2",
-    "log_r1",
-    "log_r2",
-    "log_I_plasma",
+    "k1",
+    "k2",
+    "r1",
+    "r2",
+    "I_plasma",
+    # "logit_alpha",
+    # "log_c0",
+    # "cr",
+    # "log_k1",
+    # "log_k2",
+    # "log_r1",
+    # "log_r2",
+    # "log_I_plasma",
 ]
+
+
+def standardise(expr: pl.Expr) -> pl.Expr:
+    return (expr - expr.mean()) / expr.std()
 
 
 def prepare_data(raw_data: pl.DataFrame) -> pl.DataFrame:
@@ -36,11 +50,13 @@ def prepare_data(raw_data: pl.DataFrame) -> pl.DataFrame:
                 TREATMENT_CODES.get, return_dtype=pl.String
             )
         )
-        .with_columns(mouse=pl.concat_str(pl.col("treatment"), pl.col("mouse")))
         .with_columns(
-            roi=pl.concat_str(
-                pl.col("treatment"), pl.col("mouse"), pl.col("roi")
+            mouse=pl.concat_str(
+                pl.col("treatment"), pl.col("mouse"), separator="-"
             )
+        )
+        .with_columns(
+            roi=pl.concat_str(pl.col("mouse"), pl.col("roi"), separator="-")
         )
         .with_columns(
             logit_alpha=logit(pl.col("alpha")),
@@ -50,6 +66,10 @@ def prepare_data(raw_data: pl.DataFrame) -> pl.DataFrame:
             log_r1=np.log(pl.col("r1")),
             log_r2=np.log(pl.col("r2")),
             log_I_plasma=np.log(pl.col("I_plasma")),
+        )
+        .with_columns(
+            standardise(pl.col(colname)).alias(colname + "_stnd")
+            for colname in YCOLS
         )
     )
 
@@ -61,10 +81,23 @@ def main():
     prepared_data.write_csv(PREPARED_DATA_FILE)
     print(prepared_data)
     for ycol in YCOLS:
+        x_sigma_col = ycol.replace("log_", "").replace("logit_", "") + "_std"
         print(f"Fitting dependent variable {ycol}...")
-        model = bmb.Model(
+        formula = bmb.Formula(
             MODEL_FORMULA.format(y=ycol),
+            # SIGMA_FORMULA.format(x_sigma=x_sigma_col),
+        )
+        priors = {
+            "sigma": bmb.Prior(
+                "Lognormal",
+                mu=np.log(prepared_data[ycol + "_std"].to_numpy()),
+                sigma=0.001,
+            )
+        }
+        model = bmb.Model(
+            formula,
             data=prepared_data.to_pandas(),
+            priors=priors,
         )
         idata: az.InferenceData = model.fit(target_accept=0.999)
         print(az.summary(idata))
@@ -73,7 +106,8 @@ def main():
         t: xr.DataArray = posterior["1|treatment"].sel(
             treatment__factor_dim="S"
         ) - posterior["1|treatment"].sel(treatment__factor_dim="E")
-        print((t > 0).mean())
+        p = (t > 0).mean().to_numpy().item()
+        print("Pr(saline effect > enzyme effect): " + str(round(p, 3)))
 
 
 if __name__ == "__main__":
